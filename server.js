117a7e005b1fb304eaf1f11b58af9c903c4dda16
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-04-04 23:51:50 +0700

    init code

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", (req, res)=>{
+    res.send("hello world")
+});
+
+app.use("*", (req, res) => {
+  res.status(404).json({ error: "not found" });
+});
+export default app;
