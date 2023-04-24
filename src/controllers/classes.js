2b0cad30fa29da85a80c8fab32757d60558f4b75
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-04-24 15:56:56 +0700

    add student to class

--- src/controllers/classes.js
+++ src/controllers/classes.js
@@ -6,6 +6,28 @@ const semesterModel = require("../models/semester");
 const progressModel = require("../models/progress");
 const Response = require("../utils/response");
 
+const addStudentToClass = async (classId, listStudentId, semesterId, teacherId=1) => {
+  try{
+    // create array of QuaTrinhHoc instances with HocSinhMaHS, LopMaLop, HOCKYMaHK, and GiaoVienMaGV fields
+    const progressInstances = listStudentId.map((studentId) => {
+      return {
+        StudentIdStudent: studentId,
+        ClassIdClass: classId,
+        SemesterIdSemester: semesterId,
+        TeacherIdTeacher: teacherId,
+      };
+    });
+
+    const result = await progressModel.bulkCreate(progressInstances);
+    if (result.length === progressInstances.length) return "success";
+    else return "fail";
+  } catch(err){
+    console.log("err while creating progress add student to class")
+    throw Error(err);
+  }
+ 
+};
+
 class classController {
   static async getAllClasses(req, res, next) {
     try {
@@ -50,12 +72,26 @@ class classController {
 
   static async createClass(req, res, next) {
     try {
+      
+      const {className, teacherId, semesterId, idStudentList} = req.body;
+      //check class is already exists
+      
+
+      //create class
       const newClass = classModel.build({
-        name: req.body.className,
-        number: req.body.number,
+        name: className,
+        number: idStudentList.length,
       });
-      const response = await newClass.save();
-      return res.status(200).json(Response.successResponse(response));
+      const responseNewClass = await newClass.save();
+      //add student to class
+      const result =await addStudentToClass(
+        responseNewClass.idClass,
+        idStudentList,
+        semesterId,
+        teacherId
+      );
+      console.log("result:", result);
+      return res.status(200).json(Response.successResponse(result));
     } catch (err) {
       console.log("catch err:", err);
       return res.status(404).json(Response.errorResponse(404, err.message));
@@ -95,40 +131,21 @@ class classController {
   }
 
 
-  static async addStudentToClass(req, res, next){
+  static async addStudentToClassApi(req, res, next){
     try{
-      const classId = req.params.id;
-      console.log("classId: " + classId);
-      const listStudentId = req.body.studentListId;
-      console.log("listStudentId: " + listStudentId);
-
-      //find class
-      const classInstance = await classModel.findByPk(classId);
-      console.log("classInstance: " + classInstance)
-
-      //find all students
-      const students = await studentModel.findAll({
-        where: { idStudent: listStudentId },
-      });
-      console.log("students: " + students)
-
-     // create array of QuaTrinhHoc instances with HocSinhMaHS, LopMaLop, HOCKYMaHK, and GiaoVienMaGV fields
-      const progressInstances = students.map((student) => {
-        return {
-          StudentIdStudent: student.idStudent,
-          ClassIdClass: classInstance.idClass,
-          SemesterIdSemester: 1,
-          TeacherIdTeacher: 1,
-        };
-      });
-      console.log("qth ", progressInstances);
-
-      const result = await progressModel.bulkCreate(progressInstances);
+    const {classId, listStudentId, idSemester, teacherId} = req.body;
+      //find teacher id through classId
+      const result = await addStudentToClass(
+        classId,
+        listStudentId,
+        idSemester,
+        teacherId
+      );
       console.log("result ", result);
       res.json(result);
     }catch(err){
       res.json({
-        "loi":err
+        "error":err
       });
 
     }
--- src/routes/classes.js
+++ src/routes/classes.js
@@ -4,7 +4,7 @@ const classController = require("../controllers/classes");
 
 //add new class
 route.post("/", classController.createClass);
-route.post("/:id/add", classController.addStudentToClass);
+route.post("/:id/add", classController.addStudentToClassApi);
 route.get("/", classController.getAllClasses);
 route.put("/:id", classController.updateClass);
 route.get("/:id", classController.getClassById);
