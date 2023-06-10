983ea6bcb16c5b2ddfe748e602925801ffbdecc5
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-06-10 09:50:01 +0700

    complete add new teacher

--- src/controllers/teachers.js
+++ src/controllers/teachers.js
@@ -1,14 +1,34 @@
 const teacherModel = require("../models/teacher");
+const subjectteacherModel = require("../models/subjectteacher");
 const Response = require("../utils/response");
 const { Op } = require("sequelize");
+const sequelize = require("../utils/sequelize");
+const { query } = require("express");
+const { QueryTypes } = require("sequelize");
 
 class teacherController {
   static async getAllTeachers(req, res, next) {
     try {
-      const teachers = await teacherModel.findAll();
+      let teachers = await teacherModel.findAll();
       if (!teachers) {
-        throw "Something went wrong please wait a minute and try again";
+        throw "Don't have any teacher";
       }
+      let newTeachers = [];
+      for (let i = 0; i < teachers.length; i++) {
+        //find the class of a student
+        const subjects = await sequelize.query(
+          `SELECT subject.name
+            FROM subjectteacher, subject 
+            WHERE subjectteacher.idSubject = subject.idSubject
+            AND subjectteacher.idTeacher = ${teachers[i].idTeacher}
+        `,
+          { type: QueryTypes.SELECT }
+        );
+        const subjectName = subjects.map((subject) => subject.name);
+        newTeachers.push({ ...teachers[i]["dataValues"], subjectName });
+      }
+      teachers = newTeachers;
+
       return res.status(200).json(Response.successResponse(teachers));
     } catch (err) {
       return res.status(404).json(Response.errorResponse(404, err.message));
@@ -35,8 +55,16 @@ class teacherController {
         email: req.body.Email,
         startedDay: req.body.startedDay,
       });
-      let response = await newTeacher.save();
-      return res.status(200).json(Response.successResponse(response));
+      let newTeacherRes = await newTeacher.save();
+      const subjects = req.body.Subjects;
+      console.log("subjects", subjects);
+      for(let i = 0; i < subjects.length; i++){
+        await subjectteacherModel.create({
+          idSubject: subjects[i],
+          idTeacher: newTeacherRes.idTeacher,
+        });
+      }
+      return res.status(200).json(Response.successResponse("success"));
     } catch (err) {
       return res.status(404).json(Response.errorResponse(404, err.message));
     }
@@ -59,10 +87,9 @@ class teacherController {
   }
   static async deleteTeacherById(req, res, next) {
     try {
-     
-      const response = await teacherModel.destroy({ 
-        where: { idTeacher: req.params.id } 
-      }); 
+      const response = await teacherModel.destroy({
+        where: { idTeacher: req.params.id },
+      });
       if (!response) throw "delete failed";
       return res.status(200).json(Response.successResponse(response));
     } catch (err) {
